0e7649a1eaffab57cbe4428c72e04a425fba3ac7
doedash <146763271+doedash@users.noreply.github.com>
2023-10-24 22:29:31 +0000

    Use a textarea to synth caret

--- logger/src/app/Editor.tsx
+++ logger/src/app/Editor.tsx
@@ -1,53 +1,72 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import './Editor.scss';
 
+const useFocus = () => {
+    const htmlElRef: any = useRef(null)
+    const setFocus = () => { htmlElRef.current && htmlElRef.current.focus() }
+
+    return [htmlElRef, setFocus]
+}
+
 function Editor() {
+
+    const [inputRef, setInputFocus] = useFocus()
 
     const [value, setValue] = useState("");
     const [press, setPress] = useState({ key: "" });
     const [caret, setCaret] = useState(0);
 
+    const onClick = useCallback(setInputFocus, [])
+    const onChange = useCallback((e: any) => setValue(e.target.value), [])
+    const onSelect = useCallback((e: any) => setCaret(e.target.selectionStart), [])
+
     useEffect(() => {
         document.addEventListener('keyup', (e) => {
-            console.log(e.key)
-            setPress(e);
+            // console.log(e.key)
+            // setPress(e);
         })
     }, [])
 
 
     useEffect(() => {
-        if (press.key == "Shift") {
+        // if (press.key == "Shift") {
 
-        }
-        else if (press.key == "Backspace") {
-            setValue(value.substring(0, caret - 2) + value.substring(caret - 1))
-            setCaret(caret - 1)
-        }
-        else if (press.key == "ArrowLeft") {
-            setCaret(caret - 1)
-        }
-        else if (press.key == "ArrowRight") {
-            setCaret(caret + 1)
-        }
-        else if (press.key == " ") {
-            setValue(value.substring(0, caret - 1) + "\xa0" + value.substring(caret - 1))
-            setCaret(caret + 1)
-        }
-        else {
-            setValue(value.substring(0, caret - 1) + press.key + value.substring(caret - 1))
-            setCaret(caret + 1)
-        }
+        // }
+        // else if (press.key == "Backspace") {
+        //     setValue(value.substring(0, caret - 2) + value.substring(caret - 1))
+        //     setCaret(caret - 1)
+        // }
+        // else if (press.key == "ArrowLeft") {
+        //     setCaret(caret - 1)
+        // }
+        // else if (press.key == "ArrowRight") {
+        //     setCaret(caret + 1)
+        // }
+        // else if (press.key == " ") {
+        //     setValue(value.substring(0, caret - 1) + "\xa0" + value.substring(caret - 1))
+        //     setCaret(caret + 1)
+        // }
+        // else {
+        //     setValue(value.substring(0, caret - 1) + press.key + value.substring(caret - 1))
+        //     setCaret(caret + 1)
+        // }
+        console.log(inputRef.current.selectionStart);
+
         console.log("set");
 
     }, [press])
 
+    useEffect(() => console.log(caret)
+        , [caret])
+
     return (
-        <div className="Editor">
-            <span>{value.substring(0, caret - 1)}</span>
+        <div className="Editor" onClick={onClick}>
+            <textarea className='textarea' ref={inputRef} value={value} onChange={onChange} onSelect={onSelect}></textarea>
+            <span>{value.substring(0, caret)}</span>
             <span className='caret'>
                 <span className="design"></span>
             </span>
-            <span>{value.substring(caret - 1, value.length)}</span>
+            <span>{value.substring(caret, value.length)}</span>
         </div>
     );
 }
